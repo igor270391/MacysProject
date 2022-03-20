467c016dccc4fc3d23d22fbab367c2787df6b124
Igor Senkiv <i.senkiv@mediatouch.it>
2022-03-20 22:46:36 +0100

    Add Selenium Listener, update conftest.py, add delete cookie in selenium_base.py

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -10,12 +10,20 @@ class TestHomepage:
 
     def test_nav_links(self):
         homepage_nav = HomepageNav(self.driver)
-        actual_links = homepage_nav.get_nav_links_text()
-        expected_links = homepage_nav.NAV_LINK_TEXT
-        assert expected_links == actual_links, 'Validating Nav Links text'
         homepage_nav.is_present('css', '#top>#closeButton', 'Close pop up advertise').click()
-
-        for index in range(8):
+        # homepage_nav.driver.delete_cookie('ak_bmsc')  # delete issue cookie
+        # searching cookie
+        # cookies = homepage_nav.driver.get_cookies()
+        # cookies_names = [cookie['name'] for cookie in cookies]
+        # print(cookies_names)
+        for index in range(8):  # click on each navigation link but before we dalete issue cookie (see class 'MyListener')
             homepage_nav.get_nav_links()[index].click()
-            homepage_nav.driver.delete_all_cookies()
-            time.sleep(3)
+            # homepage_nav.driver.delete_cookie('ak_bmsc')
+
+            ## we looking for cookie, which block the page and delete them
+            # for cookie_name in cookies_names:
+            #     homepage_nav.driver.delete_cookie(cookie_name)
+            #     homepage_nav.driver.refresh()
+            #     homepage_nav.is_visible('tag_name', 'h1', cookie_name)
+            #     print(cookie_name)
+            # time.sleep(1.5)
